8c474f6505f4d8eb597d07115355b5b957c45a6c
Robert Tarnas <robtar@st.amu.edu.pl>
2020-11-08 01:00:56 +0100

    Ukonczony, dzialajacy projekt 1

--- Inne projekty/Zamiana dwoch liczb/swap_projekt.c
+++ Inne projekty/Zamiana dwoch liczb/swap_projekt.c
@@ -2,38 +2,160 @@
 int main(void)
 {
 	double a,b,c,d;
+	int n;
 	scanf("%lf %lf", &a,&b);
 	
 	printf("Metoda 1:\n");
 	
 	c = a;
 	d = b;
+	
 	c = (c + d) - (d = c);
+	
 	printf("%g %g\n", c, d);
 	
 	printf("Metoda 2:\n");
 	
 	c = a;
 	d = b;
+	
 	c = d / c * (d=c);
+	
 	printf("%g %g\n", c,d);
 	
 	printf("Metoda 3:\n");
-	c = d;
+	c = a;
 	d = b;
+	
 	c=d-c+(d=c);
+	
 	printf("%g %g\n", c,d);
 	
 	printf("Metoda 4\n");
-	c = d;
+	
+	c = a;
 	d = b;
+	
 	c = d * c / (d=c);
+	
 	printf("%g %g\n", c,d);
 	
+	printf("Metoda 5\n");
 	
+	c = a;
+	d = b;
 	
+	c = (int)d ^ (int)c ^ (int)(d=c);
 	
+	printf("%d %d\n", (int)c,(int)d);
 	
+	printf("Metoda 6\n");
+	c = a;
+	d = b;
+	
+	c = c + d;
+	d = c - d;
+	c = c - d;
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 7\n");
+	
+	c = a;
+	d = b;
+	
+	c = c * d;
+	d = c / d;
+	c = c / d;
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 8\n");
+	
+	c = a;
+	d = b;
+	
+	c = c / d;
+	d = c * d;
+	c = d / c;
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 9\n");
+	
+	c = a;
+	d = b;
+	
+	c = (int)c ^ (int)d;
+	d = (int)c ^ (int)d;
+	c = (int)c ^ (int)d;
+	
+	printf("%d %d\n", (int)c, (int)d);
+	
+	printf("Metoda 10\n");
+	
+	c = a;
+	d = b;
+	
+	c = c - d;
+	d = c + d;
+	c = d - c;
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 11\n");
+	
+	c = a;
+	d = b;
+	
+	c = d - c;
+	d = d - c;
+	c = d + c;
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 12\n");
+	
+	c = a;
+	d = b;
+	
+	c = (int)d - ~((int)c) - 1;
+	d = (int)c + ~((int)d) + 1; 
+	c = (int)c + ~((int)d) + 1;
+	
+	printf("%d %d\n", (int)c, (int)d);
+	
+	printf("Metoda 13\n");
+	
+	c = a;
+	d = b;
+	
+	c = c + d; 
+	d = d - c; 
+	c = c + d; 
+	d = (-1) * d; 
+	
+	printf("%g %g\n", c,d);
+	
+	printf("Metoda 14\n");
+	
+	c = a;
+	d = b;
+	
+	c = ~((int)c ^ (int)d);
+	d = ~((int)c ^ (int)d);
+	c = ~((int)c ^ (int)d);
+	
+	printf("%d %d\n", (int)c, (int)d);
+	
+	printf("Metoda 15\n");
+	
+	c = a;
+	d = b;
+	n = 5;
+	
+	c = (c + d) - (d = c) + n - n;
+	printf("%g %g\n", c,d);	
 	
 	return 0;
 	
