9ee7086d1b4917aef4407f6a584b50db579f9cd5
Robert Tarnas <robert.tarnas@protonmail.com>
2020-12-24 16:11:52 +0100

    Update

--- Projekty ze Struktur Danych/stos&kolejka.c
+++ Projekty ze Struktur Danych/stos&kolejka.c
@@ -32,7 +32,7 @@ int stack_empty(Stack S)
 
 int push (Stack *S, int k)
 {
-	if(S->data[S->top] < 10)
+	if(S->top < 10)
 	{
 		S->data[S->top] = k;
 	}
@@ -43,9 +43,66 @@ int push (Stack *S, int k)
 	return S->top++;
 }
 
+int pop(Stack *S)
+{
+int p;
+if (S->top > 0) /* stos niepusty */
+{
+S->top--;
+p = S->data[S->top];
+S->data[S->top] = 0;
+return p;
+}
+else printf("underflow\n");
+return 0;
+}
+
+void write(Stack S)
+{
+int i;
+if(S.top == 0) printf("");
+else
+{
+for (i = 0; i < S.top; i++)
+{
+printf("%d\n", S.data[i]);
+}
+
+}
+
+
+}
+
+void write_empty(int x)
+{
+if (x == 1) printf("true\n");
+else printf("false\n");
+}
+
+void write_pop(int k)
+{
+if(k == 0) printf("");
+else printf("%d\n", k);
+}
+
 int main(void)
 {
-	Stack S;
+    Stack S;
+	init(&S);
+    push(&S,1);
+    push(&S,2);
+    write(S);
+   push(&S,3);
+   write(S);
+   write_empty(stack_empty(S));
+   write_pop(pop(&S));
+   write_pop(pop(&S));
+   write(S);
+   write_pop(pop(&S));
+   write_pop(pop(&S));
+   write(S);
+   write_empty(stack_empty(S));
+
 	return 0;
 }
 
