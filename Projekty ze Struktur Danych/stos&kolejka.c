7bbafc996372c0fd3cd48a1b67e8b4f7e4797c66
Robert Tarnas <robert.tarnas@protonmail.com>
2020-12-25 13:36:07 +0100

    Basic Queue finished

--- Projekty ze Struktur Danych/stos&kolejka.c
+++ Projekty ze Struktur Danych/stos&kolejka.c
@@ -112,6 +112,81 @@ void init_Q(Queue *Q)
     Q->head=0;
     Q->tail=0;
 }
+void Enqueue(Queue *Q, int k)
+{
+  Q->size[Q->tail]=k;
+  if (Q->tail == Q->length)
+        Q->tail = 0;
+  else
+        Q->tail++;
+
+}
+int Dequeue(Queue *Q)
+{
+   int q, i;
+   if (Q->tail == Q->head)
+   {
+    printf("underflow\n");
+    return 0;
+   }
+    else
+    {
+        q = Q->size[Q->head];
+        for(i=0;i<(Q->tail);i++)
+        {
+         Q->size[i]=Q->size[i+1];
+        }
+        Q->tail--;
+    }
+    return q;
+}
+
+int Queue_Empty(Queue Q)
+{
+ if (Q.head == Q.tail)
+ {
+return 1;
+ }
+ else return 0;
+}
+
+int write_Q(Queue Q)
+{
+	if (Q.tail == Q.head)
+    {
+        return 0;
+    }
+	else
+	{
+		int i;
+		i=Q.head;
+		while (i != Q.tail)
+		{
+			printf ("%d ", Q.size[i]);
+			if (i==Q.length)
+			{
+				i=0;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		printf("\n");
+	}
+return 0;
+}
+void write_Q_Empty(int x)
+{
+    if (x==1) printf("true\n");
+    else printf("false\n");
+}
+void write_deq(int q)
+{
+    if (q==0) printf("\n");
+    else printf("%d\n",q);
+}
+
 
 
 
@@ -140,6 +215,20 @@ int main(void)
    /*Kolejka */
 
    Queue Q;
+   init_Q(&Q);
+    Enqueue(&Q,1);
+    Enqueue(&Q,2);
+    write_Q(Q);
+    Enqueue(&Q,3);
+    write_Q(Q);
+    write_Q_Empty(Queue_Empty(Q));
+    write_deq(Dequeue(&Q));
+    write_deq(Dequeue(&Q));
+    write_Q(Q);
+    write_deq(Dequeue(&Q));
+    write_deq(Dequeue(&Q));
+    write_Q(Q);
+    write_Q_Empty(Queue_Empty(Q));
 
 
 	return 0;
